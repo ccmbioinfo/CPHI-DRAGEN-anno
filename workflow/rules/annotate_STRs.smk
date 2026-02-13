36006d65d5904d672e016b0a33c478f4b6428538
Madeline Couse <mhcouse@gmail.com>
2026-02-13 16:43:05 -0500

    merge repeat VCFs using bcftools

--- workflow/rules/annotate_STRs.smk
+++ workflow/rules/annotate_STRs.smk
@@ -1,8 +1,28 @@
-rule repeat_VCF_to_df:
+rule merge_VCFs:
     input: get_repeat_dir
+    output: "STRs/{family}.repeats.vcf.gz"
+    log: "logs/STRs/{family}.repeats.log"
+    conda: "../envs/common.yaml"
+    shell: "(bcftools merge -O z -o {output} {input}/*.repeats.vcf.gz) > {log} 2>&1"
+
+rule repeat_VCF_to_df:
+    input: 
+        vcf = "STRs/{family}.repeats.vcf.gz",
+        tbi = "STRs/{family}.repeats.vcf.gz.tbi"
     output: "STRs/{family}.repeats.tsv"
     params:
         cphi_dragen_anno = config["tools"]["cphi-dragen-anno"]
     log: "logs/repeat_VCF_to_df/{family}.repeats.log"
     conda: "../envs/annotate.yaml"
-    shell: "(python3 {params.cphi_dragen_anno}/workflow/scripts/repeat_VCF_to_df.py --repeat_vcf_dir {input} --output_file {output}) > {log} 2>&1"
+    shell: "(python3 {params.cphi_dragen_anno}/workflow/scripts/repeat_VCF_to_df.py --repeat_vcf {input.vcf} --output_file {output}) > {log} 2>&1"
+
+rule tabix:
+    input: 
+        "{prefix}.vcf.gz"
+    output: 
+        "{prefix}.vcf.gz.tbi"
+    log: 
+        "logs/{prefix}.log"
+    conda:
+           "../envs/common.yaml"
+    shell: "(tabix {input}) > {log} 2>&1"
--- workflow/scripts/repeat_VCF_to_df.py
+++ workflow/scripts/repeat_VCF_to_df.py
@@ -6,30 +6,29 @@ import numpy as np
 from pysam import VariantFile
 
 
-def main(repeat_vcf_dir, output_file):
+def main(repeat_vcf, output_file):
     with open(output_file, "w") as f:
-        for repeat_vcf in glob.glob(os.path.join(repeat_vcf_dir, "*.repeats.vcf.gz")):
-            variants = VariantFile(repeat_vcf)
-            samples = variants.header.samples
-            for rec in variants.fetch():
-                # INFO fields
-                VARID = rec.info["VARID"]
-                REF = rec.info["REF"]  # Number of repeat units spanned by the repeat in the reference
-                RL = rec.info["RL"]  # Reference length in bp
-                RU = rec.info["RU"]  # Repeat unit in the reference orientation
-                # Sample fields:
-                for sample in samples:
-                    GT = rec.samples[sample]["GT"]
-                    SO = rec.samples[sample]["SO"]  # Type of reads that support the allele. Values can be SPANNING, FLANKING, or INREPEAT. These values indicate if the reads span, flank, or are fully contained in the repeat.
-                    REPCN = rec.samples[sample]["REPCN"]  # Number of repeat units spanned by the allele
-                    REPCI = rec.samples[sample]["REPCI"]  # Confidence interval for REPCN
-                    ADSP = rec.samples[sample]["ADSP"]  # Number of spanning reads consistent with the allele
-                    ADFL = rec.samples[sample]["ADFL"]  # Number of flanking reads consistent with the allele
-                    ADIR = rec.samples[sample]["ADIR"]  # Number of in-repeat reads consistent with the allele
-                    LC = rec.samples[sample]["LC"]  # Locus Coverage
-                    f.write(
-                        f"{sample}\t{rec.chrom}\t{rec.pos}\t{VARID}\t{REF}\t{RL}\t{RU}\t{GT}\t{SO}\t{REPCN}\t{REPCI}\t{ADSP}\t{ADFL}\t{ADIR}\t{LC}\n"
-                    )
+        variants = VariantFile(repeat_vcf)
+        samples = variants.header.samples
+        for rec in variants.fetch():
+            # INFO fields
+            VARID = rec.info["VARID"]
+            REF = rec.info["REF"]  # Number of repeat units spanned by the repeat in the reference
+            RL = rec.info["RL"]  # Reference length in bp
+            RU = rec.info["RU"]  # Repeat unit in the reference orientation
+            # Sample fields:
+            for sample in samples:
+                GT = rec.samples[sample]["GT"]
+                SO = rec.samples[sample]["SO"]  # Type of reads that support the allele. Values can be SPANNING, FLANKING, or INREPEAT. These values indicate if the reads span, flank, or are fully contained in the repeat.
+                REPCN = rec.samples[sample]["REPCN"]  # Number of repeat units spanned by the allele
+                REPCI = rec.samples[sample]["REPCI"]  # Confidence interval for REPCN
+                ADSP = rec.samples[sample]["ADSP"]  # Number of spanning reads consistent with the allele
+                ADFL = rec.samples[sample]["ADFL"]  # Number of flanking reads consistent with the allele
+                ADIR = rec.samples[sample]["ADIR"]  # Number of in-repeat reads consistent with the allele
+                LC = rec.samples[sample]["LC"]  # Locus Coverage
+                f.write(
+                    f"{sample}\t{rec.chrom}\t{rec.pos}\t{VARID}\t{REF}\t{RL}\t{RU}\t{GT}\t{SO}\t{REPCN}\t{REPCI}\t{ADSP}\t{ADFL}\t{ADIR}\t{LC}\n"
+                )
 
 
 if __name__ == "__main__":
@@ -37,14 +36,14 @@ if __name__ == "__main__":
         description="Converts DRAGEN repeat VCF(s) to a TSV"
     )
     parser.add_argument(
-        "--repeat_vcf_dir", type=str, help="DRAGEN repeat VCF", required=True
+        "--repeat_vcf", type=str, help="DRAGEN repeat VCF", required=True
     )
     parser.add_argument(
         "--output_file", type=str, help="Output filename", required=True
     )
 
     args = parser.parse_args()
-    repeat_vcf_dir = args.repeat_vcf_dir
+    repeat_vcf = args.repeat_vcf
     output_file = args.output_file
 
-    main(repeat_vcf_dir, output_file)
+    main(repeat_vcf, output_file)
